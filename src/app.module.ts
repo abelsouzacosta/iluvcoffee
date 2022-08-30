35887447eb9a3882e9d64b7f171dbd4af24f03e0
junior <abelsouzacosta@gmail.com>
2022-08-30 14:27:55 -0300

    refactor(application): settings
    setting typeorm and postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: 'db',
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+    }),
+    CoffeesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
