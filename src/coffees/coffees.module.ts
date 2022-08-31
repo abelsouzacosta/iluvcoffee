cfd55bec4210a7185bbaf1fda06b57f221399fd4
junior <abelsouzacosta@gmail.com>
2022-08-31 14:13:53 -0300

    chore(cofee): repository
    register repository to module

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -3,10 +3,12 @@ import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
+import { CoffeeRepository } from './domain/repositories/coffee.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee])],
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [CoffeesService, CoffeeRepository],
 })
 export class CoffeesModule {}
