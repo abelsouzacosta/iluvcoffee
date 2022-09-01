a4a2869e6a80fef5dc6927dbd421e23376a8799a
junior <abelsouzacosta@gmail.com>
2022-09-01 01:44:21 -0300

    chore(coffees): service
    adding repository to service

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,32 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { CreateCoffeeDto } from './domain/dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './domain/dto/update-coffee.dto';
 import { CoffeeRepository } from './domain/repositories/coffee.repository';
-import { CreateCoffeeDto } from './dto/create-coffee.dto';
-import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Injectable()
 export class CoffeesService {
   constructor(private readonly repository: CoffeeRepository) {}
 
-  async create(data: CreateCoffeeDto) {
+  create(data: CreateCoffeeDto) {
     return this.repository.create(data);
   }
 
-  findAll(query: PaginationQueryDto) {
-    return this.repository.find(query);
+  findAll() {
+    return this.repository.find();
   }
 
-  async findOne(id: number) {
-    const coffee = await this.repository.findById(id);
-
-    return coffee;
+  findOne(id: number) {
+    return this.repository.findById(id);
   }
 
-  async update(id: number, data: UpdateCoffeeDto) {
+  update(id: number, data: UpdateCoffeeDto) {
     return this.repository.update(id, data);
   }
 
-  async remove(id: number) {
+  remove(id: number) {
     return this.repository.remove(id);
   }
 }
