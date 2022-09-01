e54c299df70168e1e4d9355ab036f3dbc8db9de6
junior <abelsouzacosta@gmail.com>
2022-09-01 10:06:04 -0300

    refactor(flavors): entity
    register entity to the module

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,9 +4,10 @@ import { CoffeesController } from './coffees.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { CoffeeRepository } from './domain/repositories/coffee.repository';
+import { Flavor } from './entities/flavors.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee])],
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
   controllers: [CoffeesController],
   providers: [CoffeesService, CoffeeRepository],
 })
