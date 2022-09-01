3da4a1b0f20fbcadf11cbb593c8c8b06a03fd114
junior <abelsouzacosta@gmail.com>
2022-09-01 18:52:27 -0300

    refactor(auth): guard
    refactor guard to deal with public endpoints

--- src/common/guards/api-key.guard.ts
+++ src/common/guards/api-key.guard.ts
@@ -7,12 +7,18 @@ import {
 import { Request } from 'express';
 import { Observable } from 'rxjs';
 import 'dotenv/config';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler());
+
     const ctx = context.switchToHttp();
 
     const request = ctx.getRequest<Request>();
@@ -24,8 +30,8 @@ export class ApiKeyGuard implements CanActivate {
 
     const [, token] = header.split(' ');
 
-    if (token !== process.env.API_KEY) return false;
+    if (token === process.env.API_KEY || isPublic) return true;
 
-    return true;
+    return false;
   }
 }
