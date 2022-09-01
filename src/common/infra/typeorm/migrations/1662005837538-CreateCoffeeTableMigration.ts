53570a0ecfe49a0071744cb2a2b29ee922c6b436
junior <abelsouzacosta@gmail.com>
2022-09-01 01:42:34 -0300

    chore(coffees): migration
    adding coffees migration table

---
+++ src/common/infra/typeorm/migrations/1662005837538-CreateCoffeeTableMigration.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCoffeeTableMigration1662005837538
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'coffees',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'brand',
+            type: 'varchar',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('coffees');
+  }
+}
