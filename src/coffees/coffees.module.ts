e2c43357a9e7cac68774b75bb7764b15435177f8
junior <abelsouzacosta@gmail.com>
2022-09-02 07:11:47 -0300

    feat(app): request
    add middleware that returns request time

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { CoffeeRepository } from './domain/repositories/coffee.repository';
 import { Flavor } from './entities/flavors.entity';
+import { LoggingMiddleware } from '../common/middlewares/logging.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
   controllers: [CoffeesController],
   providers: [CoffeesService, CoffeeRepository],
 })
-export class CoffeesModule {}
+export class CoffeesModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('coffees');
+  }
+}
